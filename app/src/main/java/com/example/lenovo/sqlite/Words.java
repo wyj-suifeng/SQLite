a72038b19d8201edf0fbad56ddadc8bc02acc3d2
wyj-suifeng <1041457754@qq.com>
2016-09-07 22:38:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/sqlite/Words.java
@@ -0,0 +1,40 @@
+package com.example.lenovo.sqlite;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Words {
+    public static final String AUTHORITY = "com.example.lenovo.sqlite";//URI授权者
+    public Words(){
+
+    }
+    public static abstract class Word implements BaseColumns{
+        public static final String TABLE_NAME="words";
+        public static final String COLUMN_NAME_WORD="word";//列：单词
+        public static final String COLUMN_NAME_MEANING="meaning";//列：单词含义
+        public static final String COLUMN_NAME_SAMPLE="sample";//单词示例
+
+        //MIME类型
+        public static final String MIME_DIR_PREFIX = "vnd.android.cursor.dir";
+        public static final String MIME_ITEM_PREFIX = "vnd.android.cursor.item";
+        public static final String MINE_ITEM = "vnd.example.lenovo.word";
+
+        public static final String MINE_TYPE_SINGLE = MIME_ITEM_PREFIX + "/" + MINE_ITEM;
+        public static final String MINE_TYPE_MULTIPLE = MIME_DIR_PREFIX + "/" + MINE_ITEM;
+
+        public static final String PATH_SINGLE = "word/#";//单条数据的路径
+        public static final String PATH_MULTIPLE = "word";//多条数据的路径
+
+        //Content Uri
+        public static final String CONTENT_URI_STRING = "content://" + AUTHORITY + "/" + PATH_MULTIPLE;
+        public static final Uri CONTENT_URI = Uri.parse(CONTENT_URI_STRING);
+    }
+}
+
+
